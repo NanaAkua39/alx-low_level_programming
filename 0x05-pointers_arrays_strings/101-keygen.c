423848252f3bee7eefc209602618d19b668e9095
NanaAkua39 <nanaekua39@gmail.com>
2023-02-27 10:58:53 -0800

    Task 12

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -13,8 +13,8 @@ int main(void)
 	int pass[100];
 	int i, sum, n;
 
-	sum = 0;	
-
+	sum = 0;
+	
 	srand(time(NULL));
 
 	for (i = 0; i < 100; i++)
@@ -33,3 +33,4 @@ int main(void)
 
 	return (0);
 }
+
